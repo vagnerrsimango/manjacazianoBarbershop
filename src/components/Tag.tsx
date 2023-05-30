8dfb11ecdabbfed109ceb6d618f570b4c967a13b
Victor Panguene <svpanguene@gmail.com>
2023-05-30 20:04:52 +0200

    tags

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,17 +1,20 @@
+import { DotsNine, Scissors, TagSimple } from 'phosphor-react-native';
 import MyButton from './MyButton';
+import { useTheme } from 'native-base';
 interface ITag {
   title: string;
 }
 export default function Tag({ title }: ITag) {
+  const { colors } = useTheme();
   return (
     <MyButton
-      fontSize={10}
       title={title}
       bg="muted.400"
       mr={2}
       rounded={'4'}
       weight="700"
-      height={8}
+      height={10}
+      leftIcon={<Scissors color={colors.white} size={16} />}
     />
   );
 }
