0956c22473b6845bee5a29a8b02506f72cf8128b
Elton Vilanculo <eltonvillas10@gmail.com>
2023-06-21 17:07:02 +0200

    upated base url

--- src/utils/network/api.ts
+++ src/utils/network/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 // baseURL:'http://apimanjacaziano.vaeio.co'
 const api = axios.create({
-  baseURL: "http://192.168.43.55:7777",
+  baseURL: "http://apimanjacaziano.vaeio.co",
 });
 
 export default api;
