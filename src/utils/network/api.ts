72b1159fa51e49ce988a6fca5cf039921fa2c9ed
Elton Vilanculo <eltonvillas10@gmail.com>
2023-07-17 11:16:05 +0200

    presentation

--- src/utils/network/api.ts
+++ src/utils/network/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:'http://apimanjacaziano.vaeio.co'
-})
+  baseURL: "http://apimanjacaziano.gestao.site",
+});
 
 export default api;
