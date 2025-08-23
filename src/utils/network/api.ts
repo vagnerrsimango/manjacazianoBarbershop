b235a6c6e42c1a9bceb671577c62d917162226a3
Elton Vilanculo <eltonvillas10@gmail.com>
2025-08-23 23:21:37 +0200

    updated endpoint

--- src/utils/network/api.ts
+++ src/utils/network/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://192.168.18.2:7777",
-  baseURL: "http://apimanjacaziano.gestaosistema.com",
+  baseURL: "https://apimanjacaziano.geome.site",
 });
 
 // TODO: Order sales by date, filter by dare
